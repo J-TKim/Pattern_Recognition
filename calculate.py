b300935d3b7fd30ff3c37bc9b5da30c156e11481
Kim, Jeong Tae <68172322+J-TKim@users.noreply.github.com>
2021-04-14 16:19:19 +0900

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import math
+
+
+def calculate_roundness(image_path):
+    img = cv2.imread(image_path)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    # noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(
+        dist_transform, 0.7*dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]
+
+    sure_bg = sure_bg.astype(np.uint8)
+
+    ret, thresh = cv2.threshold(sure_bg, -1, 1, 0)
+    contours, hierarchy = cv2.findContours(sure_bg, 1, 2)
+
+    cnt = contours[0]
+    M = cv2.moments(cnt)
+
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    area = cv2.contourArea(cnt)
+
+    perimeter = cv2.arcLength(cnt, True)
+
+    epsilon = 0.001 * cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, epsilon, True)
+
+    A_p = area
+    A_c = radius ** 2 * math.pi
+
+    R = A_p / A_c * 100
+    return R
